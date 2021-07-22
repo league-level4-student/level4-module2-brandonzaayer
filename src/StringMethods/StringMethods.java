b2fa555e8570b0fbca18356b5e466a033d5324bf
brandonzaayer <33267170+brandonzaayer@users.noreply.github.com>
2021-07-21 17:32:15 -0700

    working with strings

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,30 +32,34 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if(s1.length() > s2.length()) {
+			return s1;
+		}
+		else {
+			return s2;
+		}
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
+		
 		return null;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
 		return null;
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
 		return 0;
 	}
-	
-	
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
 		return 0;
@@ -71,13 +75,11 @@ public class StringMethods {
 		return null;
 	}
 
-
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
 		return 0;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
@@ -86,17 +88,30 @@ public class StringMethods {
 		return 0;
 	}
 
-
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
-		return true;
+		String forward = "";
+		for (int i = s.length()-1; i >= 0; i--) {
+			if (s.charAt(i) != ' ' && Character.isLetter(i)) {
+				forward = forward + s.charAt(i);
+			}
+		}
+		String noSpaces = "";
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != ' ' && Character.isLetter(i)) {
+				noSpaces = noSpaces + s.charAt(i);
+			}
+		}
+		if (noSpaces.equalsIgnoreCase(forward)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
-	
-}
 
-class Utilities {
+static class Utilities {
 	// This basic encryption scheme is called single-byte xor. It takes a single
 	// byte and uses exclusive-or on every character in the String.
 	public static String encrypt(byte[] plaintext, byte key) {
@@ -113,4 +128,5 @@ class Utilities {
 		}
 		return new String(b);
 	}
+}
 }
